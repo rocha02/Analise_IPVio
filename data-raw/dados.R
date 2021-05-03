6e2686c63428783936cb7b976db7e970488b18b0
Rafel Rocha <rochaunit02@gmail.com>
2021-05-02 22:12:40 -0300

    Initial commit

---
+++ data-raw/dados.R
@@ -0,0 +1,25 @@
+## code to prepare `dados` dataset goes here
+
+## Importação das bases do IPVIO 2021
+
+ipvio_MA <- readxl::read_xlsx("IPVio_2021_MARIO_ANDREAZZA.xlsx")
+ipvio_SA <- readxl::read_xlsx("IPVio_2021_SANTO_AMARO.xlsx")
+
+## Limpeza de colunas iniciais 
+
+ipvio_MA <- ipvio_MA[, -c(1:13)] # deletar colunas 1 até 13 
+ipvio_SA <- ipvio_SA[, -c(1:13)] # deletar colunas 1 até 13 
+
+## Limpeza de nomes das colunas
+
+glimpse(ipvio_MA)
+
+ipvio_MA <- clean_names(ipvio_MA)
+ipvio_SA <- clean_names(ipvio_SA)
+
+## Soma dos bancos da PB e do PE
+
+ipvio_2021 <- rbind(ipvio_MA, ipvio_SA)
+
+
+usethis::use_data(dados, overwrite = TRUE)
